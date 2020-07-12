c6d61915026de74f927985049231a71c26520383
kkoliou <56164783+kkoliou@users.noreply.github.com>
2020-07-12 23:51:16 +0300

    remove currency utils

--- app/src/main/java/com/android/calculator/CurrencyExchangeService.java
+++
@@ -1,9 +0,0 @@
-package com.android.calculator;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface CurrencyExchangeService {
-    @GET("http://data.fixer.io/api/latest?access_key=cf2cec699bac6a877687217edc984666")
-    Call<CurrencyExchange> loadCurrencyData();
-}
--- app/src/main/java/com/android/calculator/CurrencyFrag.java
+++ app/src/main/java/com/android/calculator/CurrencyFrag.java
@@ -4,51 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-import static android.widget.Toast.LENGTH_LONG;
-
-public class CurrencyFrag extends Fragment implements Callback<CurrencyExchange> {
+public class CurrencyFrag extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.currency_frag, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        loadCurrencyData();
-    }
-
-    private void loadCurrencyData() {
-        Retrofit retrofit = new Retrofit.Builder()
-                                .baseUrl("http://data.fixer.io/api/latest?access_key=cf2cec699bac6a877687217edc984666")
-                                .addConverterFactory(GsonConverterFactory.create())
-                                .build();
-
-        CurrencyExchangeService service = retrofit.create(CurrencyExchangeService.class);
-        Call<CurrencyExchange> call = service.loadCurrencyData();
-        call.enqueue(this);
-    }
-
-    @Override
-    public void onResponse(Call<CurrencyExchange> call, Response<CurrencyExchange> response) {
-        Toast.makeText(getContext(), "temp", LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onFailure(Call<CurrencyExchange> call, Throwable t) {
-        Toast.makeText(getContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
     }
 }
